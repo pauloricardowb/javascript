c46a980771fe2704ba512f3a92bcd98a46bd12ca
Paulo Ricardo <ricardo09s@proton.me>
2025-07-21 15:24:36 -0300

    JavaScript Exercises (Part 6)

--- cursojavascript/aula014ex/ex017/script.js
+++ cursojavascript/aula014ex/ex017/script.js
@@ -1,1 +1,19 @@
-alert("Olá!")
+function calcular() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.length == 0) {
+        window.alert('Digite um número!')
+    } else {
+        var n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''//para limpar a tabuada
+        while (c <= 10) {//estrutura de repetição
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` //para saber qual item foi selecionado
+            tab.appendChild(item)//para exibir a tabuada
+            c++
+        }  
+    }
+}
