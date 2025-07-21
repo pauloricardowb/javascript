9d702e6c44fbf003fc56ff7e5e62727671034b26
Paulo Ricardo <ricardo09s@proton.me>
2025-07-21 08:54:12 -0300

    Loopings (Part 1)

---
+++ cursojavascript/aula013/ambiente.js
@@ -0,0 +1,24 @@
+// Repetições
+
+var c = 1 // Contador
+do {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+} while (c <= 6)
+
+/*
+var c = 1 // Contador
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
