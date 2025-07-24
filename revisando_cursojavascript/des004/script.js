f95283260dbfd863ecb8c7c420bb35e64dc68a11
Paulo Ricardo <ricardo09s@proton.me>
2025-07-24 08:55:02 -0300

    Revisando desafios

--- revisando_cursojavascript/des004/script.js
+++ revisando_cursojavascript/des004/script.js
@@ -1,9 +1,18 @@
 function calcular() {
-    prod = window.prompt('Está comprando qual produto?')
-    prec = Number(window.prompt(`Qual o preço do ${prod}`))
-    val = Number(window.prompt(`Qual o valor dado pelo ${prod}`))
-
-    troco = val - prec
-    
-    window.alert(`Você pagou ${prec} no produto ${prod} e vai receber um troco de ${troco}. Volte sempre!`)
+    p = window.prompt(`Quer usar o programa? (sim) ou (não)`)
+    if (p.toLowerCase() == "sim") {
+        prod = window.prompt('Está comprando qual produto?')
+        prec = Number(window.prompt(`Qual o preço do ${prod}`))
+        val = Number(window.prompt(`Qual o valor dado pelo ${prod}`))
+        troc = val - prec
+        if (prod == "" || prec == "" || val == "") {
+            alert('Tchau!')
+        } else if (val >= prec) {
+            alert(`Você comprou o produto ${prod} que custa o preço R$${prec.toFixed(2)} e recebeu de troco R$${troc.toFixed(2)}`)
+        } else {
+            window.alert('Não vendemos fiado!')
+        }  
+    } else {
+        alert('Volte outra hora então!')
+    }
 }
