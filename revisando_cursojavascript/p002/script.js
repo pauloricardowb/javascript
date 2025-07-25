361799560db93c78872c1f9c4aefc331457dee16
Paulo Ricardo <ricardo09s@proton.me>
2025-07-25 08:34:00 -0300

    Corrigido bug da aplicação

--- revisando_cursojavascript/p002/script.js
+++ revisando_cursojavascript/p002/script.js
@@ -25,25 +25,28 @@ function verificar() {
                 res.innerHTML = `<p>Viajantes do tempo, algumas pessoas não acreditam neles! imagine só, sendo que há um, agora mesmo, acessando esse site. Ou seria só uma pessoa buscando erros na aplicação? \u{1f914}</p>`
             } else if (idade >= 0 && idade < 10) {
                 img.setAttribute('src', 'imagens/criancamas.jpg')
+                res.innerHTML =''
             } else if (idade < 21) {
                 img.setAttribute('src', 'imagens/jovemmas.jpg')
-                p = prompt('Gostaria de uma dica rapaz?')
-                if (p.toLowerCase() = "sim") {
+                p = prompt('Gostaria de uma dica rapaz? (sim ou não)')
+                if (p.toLowerCase() == "sim") {
                     res.innerHTML = '<p>Honre pai e mãe, mesmo que possivelmente já seja independente.</p>'
                 } else {
                     res.innerHTML = '<p>Ok!</p>'
                 }                    
             } else if (idade < 50) {
                 img.setAttribute('src', 'imagens/adultomas.jpg')
+                res.innerHTML =''
             } else {
                 img.setAttribute('src', 'imagens/idosomas.jpg')
-            } 
+                res.innerHTML ='' //para corrigir bug de concatenar respostas
+            }
         } else if (fsex[1].checked) {
             alert('Olá minha cara!')
             genero = "Mulher"
             if (idade > 120) {
                 img.setAttribute('src', 'imagens/ancientfem.jpg')
-                res.innerHTML = `<p>"Os imortais, pessoas que vivem muito mais do que as outras, ou será se é só alguém querendo causar um comportamento incomum no programa?" \u{1f914}</p>`
+                res.innerHTML += `<p>"Os imortais, pessoas que vivem muito mais do que as outras, ou será se é só alguém querendo causar um comportamento incomum no programa?" \u{1f914}</p>`
             } else if (idade < 0) {
                 img.setAttribute('src', 'imagens/travaler.jpg')
                 document.body.style.background = "black"
@@ -51,6 +54,7 @@ function verificar() {
                 res.innerHTML = `<p>Viajantes do tempo, algumas pessoas não acreditam neles! imagine só, sendo que há um, agora mesmo, acessando esse site. Ou seria só uma pessoa buscando erros na aplicação? \u{1f914}</p>`
             } else if (idade >= 0 && idade < 10) {
                 img.setAttribute('src', 'imagens/criancafem.jpg')
+                res.innerHTML = ''
             } else if (idade >= 0 && idade < 21) {
                 img.setAttribute('src', 'imagens/jovemfem.jpg')
                 p = prompt('Gostaria de uma dica moça? "sim ou não"')
@@ -61,8 +65,10 @@ function verificar() {
                 }
             } else if (idade < 50) {
                 img.setAttribute('src', 'imagens/adultofem.jpg')
+                res.innerHTML = ''
             } else {
                 img.setAttribute('src', 'imagens/idosofem.jpg')
+                res.innerHTML = ''//para corrigir bugs de concatenar respostas
             }
         }
         res.style.textAlign = 'center'
